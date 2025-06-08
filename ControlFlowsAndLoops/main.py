8d8f5cd92abd70798fbbc5a63f3ae519ae09d379
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2025-06-09 02:35:33 +0500

    Refactor commented-out code for multiplication and enhance match-case structure in main.py

--- ControlFlowsAndLoops/main.py
+++ ControlFlowsAndLoops/main.py
@@ -12,9 +12,19 @@ else:
 # for i in range(1, 11):
 #     print(f"5 x {i} = {5 * i}")
 
-i = 1
-while i <= 10:
-    print(f"5 x {i} = {5 * i}")
-    i += 1
+# i = 1
+# while i <= 10:
+#     print(f"5 x {i} = {5 * i}")
+#     i += 1
 
-    
+c = 32
+
+match c:
+    case 1:
+        print("c is 1")
+    case 2:
+        print("c is 2")
+    case 3:
+        print("c is 3")    
+    case _:
+        print("c is something else")
