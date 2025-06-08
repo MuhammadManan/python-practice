60bafe30f5f21d77d4b576d6928aa650c1bc8f9f
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2025-06-09 02:27:55 +0500

    Refactor age input handling and improve code formatting in main.py

--- ControlFlowsAndLoops/main.py
+++ ControlFlowsAndLoops/main.py
@@ -1,3 +1,4 @@
+'''
 age = int(input("Enter your age: "))  # Convert input to integer
 if age < 18:
     print("You are a minor.")
@@ -5,3 +6,15 @@ elif age == 18:
     print("You are exactly 18 years old.")
 else:
     print("You are an adult.")
+
+'''
+
+# for i in range(1, 11):
+#     print(f"5 x {i} = {5 * i}")
+
+i = 1
+while i <= 10:
+    print(f"5 x {i} = {5 * i}")
+    i += 1
+
+    
