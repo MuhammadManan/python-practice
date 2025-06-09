c995c05e08740e7b5d500c9dee8335fcaa93219c
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2025-06-09 19:35:45 +0500

    Comment out average function and its usage; add lambda function for summation

--- FunctionsAndModules/main.py
+++ FunctionsAndModules/main.py
@@ -1,7 +1,10 @@
-def average(a,b,c):
+"""def average(a,b,c):
     return (a + b + c) / 3
 
 o1 = average(10, 20, 30)
 o2 = average(5, 15, 25)
 
-print(int(o1+o2))
+print(int(o1+o2))"""
+
+sum = lambda a, b: a + b
+print(sum(10, 20))
