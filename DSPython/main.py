aa787352da9a4aa3eee51acbe3669c30182aab50
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2025-06-09 23:46:58 +0500

    Refactor marks list operations: insert value and push function implementation

--- DSPython/main.py
+++ DSPython/main.py
@@ -1,5 +1,13 @@
 marks = [23,45,67,89,12,34,56,78,90,100]
-print("Marks:", len(marks))
-print(marks[2:5])
-print(marks[2:5:2])  # Slicing with step
-print(marks[8:2:-1])  # Slicing with negative step
+# print("Marks:", len(marks))
+# print(marks[2:5])
+# print(marks[2:5:2])  # Slicing with step
+# print(marks[8:2:-1])  # Slicing with negative step
+
+def push(lst, value):
+    lst.append(value)
+
+marks.insert(2, 69)
+print("Marks after insertion:", marks)
+push(marks, 432)
+print("Marks after push:", marks)
