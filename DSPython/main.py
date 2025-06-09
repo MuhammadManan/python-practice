d49883e01f7fc2b12330a76c0797252ebd0f74a5
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2025-06-09 23:40:05 +0500

    Add initial implementation of marks list and slicing examples in main.py

---
+++ DSPython/main.py
@@ -0,0 +1,5 @@
+marks = [23,45,67,89,12,34,56,78,90,100]
+print("Marks:", len(marks))
+print(marks[2:5])
+print(marks[2:5:2])  # Slicing with step
+print(marks[8:2:-1])  # Slicing with negative step
