8a0baa1f2b267851d1bcaea5f5c914dde0d4346c
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2025-06-12 02:48:53 +0500

    Refactor main.py to properly document the requests library usage and add regex functionality for text matching

--- WorkingWithExternalLibraries/main.py
+++ WorkingWithExternalLibraries/main.py
@@ -1,4 +1,4 @@
-#  working with requests library
+'''#  working with requests library
 import requests
 
 Response = requests.get("https://api.github.com/users/octocat")
@@ -6,4 +6,17 @@ Response = requests.get("https://api.github.com/users/octocat")
 # print(Response.text)
 
 with open("octocat.json", "w") as file:
-    file.write(Response.text)
+    file.write(Response.text)'''
+
+
+import re
+# working with re module
+text = "The quick brown fox jumps over the lazy dog."
+match = re.search("brown", text)
+print(match)
+if match:
+    print("Match found:", match.group())
+    print(match.start())
+    print(match.end())
+else:
+    print("No match found.")
